129b2c07daca95ca61e1c314cc688566b0bb52ca
rolme <roland.parnaso@gmail.com>
2013-03-15 01:07:12 -0700

    adding generated files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spoodle::Application.config.secret_token = 'f41c7600a6425599d592ddfe572bd585288c3b292c1d128dc245437ff87d74d06f150b0cf421418eca39cdd272216d4b4f7df0eb4b56e4be7566f2b12408673a'
